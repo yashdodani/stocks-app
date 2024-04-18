734e5995fe9f24abee5a11a8b5003009046a2cd2
Yash Dodani <yashdodani8@gmail.com>
2024-04-18 11:59:46 +0530

    fetching and storage

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const axios = require('axios');
+const morgan = require('morgan');
+const stockSerivce = require('./controllers/stocks');
 
 const app = express();
+app.use(morgan('dev'));
 app.use(express.json());
+
+app.use('/api', stockSerivce);
 
 module.exports = app;
--- backend/controllers/stocks.js
+++ backend/controllers/stocks.js
@@ -1,5 +1,84 @@
-const stocks = JSON.parse(
-    fs.readFileSync(`${__dirname}/../dev-data/stocks.json`)
-);
+const fs = require('fs');
+const axios = require('axios');
+const router = require('express').Router();
 
-console.log(stocks);
+// const dbPath = '../data/stocks.json';
+
+const listOfTickers = [
+    'MSFT',
+    'AAPL',
+    'NVDA',
+    'GOOG',
+    'AMZN',
+    'META',
+    'JPM',
+    'WMT',
+    'TSLA',
+    'BRK/A',
+    'BRK/B',
+    'TSM',
+    'LLY',
+    'AVGO',
+    'NVO',
+    'V',
+    'MA',
+    'JNJ',
+    'ORCL',
+    'HD',
+];
+
+// const stocksData = JSON.parse(
+//     fs.readFileSync(`${__dirname}/../data/stocks.json`)
+// );
+
+const fetchData = async (ticker) => {
+    try {
+        const url = `https://api.polygon.io/v2/aggs/ticker/${ticker}/range/1/day/2023-01-09/2023-01-09?apiKey=ywfYJ3C_S23CpIHnl6XbM6z551QsPSqF`;
+        const response = await axios.get(url);
+        return response.data;
+    } catch (err) {
+        console.log(err.message);
+    }
+};
+
+// const saveDataToDB = (data) => {
+//     const newId = stocksData.length + 1;
+//     const newStock = Object.assign({
+//         id: newId,
+//         ticker: data.ticker,
+//         prices: data.results,
+//     });
+
+//     console.log(stocksData);
+//     fs.writeFileSync(
+//         `${__dirname}/../data/stocks.json`,
+//         JSON.stringify(stocksData)
+//     );
+// };
+
+router.get('/', (req, res) => {
+    res.status(200).send('hello from server');
+});
+
+router.get('/stocks/:n', async (req, res) => {
+    const numberOfTickers = Number(req.params.n);
+    console.log(numberOfTickers);
+    const tickers = listOfTickers.slice(0, numberOfTickers);
+    console.log(tickers);
+    try {
+        const promiseArray = tickers.map((ticker) => fetchData(ticker));
+        const fullData = await Promise.all(promiseArray);
+        fs.writeFileSync(
+            `${__dirname}/../data/stocks.json`,
+            JSON.stringify(fullData)
+        );
+
+        res.status(200).send(fullData);
+    } catch (err) {
+        console.log(err);
+        console.log(err.message);
+        res.status(500).end();
+    }
+});
+
+module.exports = router;
